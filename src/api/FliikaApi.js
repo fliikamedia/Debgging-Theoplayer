9139492b657a03d4e592d28d437b7ae61d8ab2fb
Youcef Ben Abdessadek <youcef.ben@fliika.com>
2021-08-06 09:59:17 +0000

    Deleted src

--- src/api/FliikaApi.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://fliika-api-back.azurewebsites.net",
-});
--- src/api/expressApi.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://fliika-user-api.azurewebsites.net/",
-});
