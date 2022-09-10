c6b3487b704e52677e57f1543e290329f878801a
YoucefBen47 <70854871+YoucefBen47@users.noreply.github.com>
2022-09-10 19:25:53 +0100

    New Profile Images

---
+++ src/MobileMoney.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MobileMoney = () => {
+  const exchange_url = `https://api.apilayer.com/exchangerates_data/convert?to=${userCurrency}&from=USD&amount=${conversionAmount}`;
+  const key = "BDNDzZTdhlTDbVM6HAjERXBTHJcGPWOw";
+  return <div>MobileMoney</div>;
+};
+
+export default MobileMoney;
