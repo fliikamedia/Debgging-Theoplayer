ab5ae4d3ebab30dcfb68b54e4fd908e5118ddbfc
YoucefBen47 <70854871+YoucefBen47@users.noreply.github.com>
2022-09-17 15:30:53 +0100

    Using new geolocation api

--- src/api/geoLocationApi.js
+++ src/api/geoLocationApi.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://ipgeolocation.abstractapi.com/v1/?api_key=1a9aca489f7a4011bf341eb6c3883062",
+    // "https://ipgeolocation.abstractapi.com/v1/?api_key=1a9aca489f7a4011bf341eb6c3883062",
+    "https://api.ipdata.co/?api-key=2c7b6a05daec7950cc0b3d7fd70d0bc08761644db9d5191b0dafcd12",
 });
